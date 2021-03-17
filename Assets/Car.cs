76fe46739e995324b083e35709be799cfc6446f2
Alberto Servín <aservins@gmail.com>
2021-03-16 18:16:07 -0600

    Initial commit

---
+++ Assets/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    //-1 = izquierda
+    //0 = en medio 
+    //1 = derecha
+    int rail = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && rail != -1)
+        {
+            rail--;
+            transform.position = new Vector2(transform.position.x - 1.65f, transform.position.y);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && rail != 1)
+        {
+            rail++;
+            transform.position = new Vector2(transform.position.x + 1.65f, transform.position.y);
+        }
+    }
+}
---
+++ Assets/MoveRail.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRail : MonoBehaviour
+{
+    float moveSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -9)
+        {
+            transform.position = new Vector2(0, 9.5f);
+        }
+
+        transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+    }
+}
